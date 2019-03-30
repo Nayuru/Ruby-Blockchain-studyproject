242fda24f5414bf3d5caf83b4e4310a51dcc22be
Antoine <aanthime@gmail.com>
2019-03-30 11:21:05 +0100

    Blockchain-Ruby-Implementation
    Encore besoin de trouver une meilleure implémentation de la difficulté, car là la le minage est O(n²), ça va pas être possible.

---
+++ RubyCoinsBlockchain/blockchainlib.rb
@@ -0,0 +1,119 @@
+require 'digest'
+require 'date'
+#--------------------------------------------------------------------------
+class Transaction
+  attr_accessor :sender, :amount, :receiver
+  def initialize(sender, amount, receiver)
+    @sender = sender
+    @amount = amount
+    @receiver = receiver
+  end
+end
+#--------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------
+class Block
+  attr_accessor :index, :timestamp, :data, :previous_hash, :hash
+  def initialize(index, timestamp, data, previous_hash)
+    @index = index
+    @timestamp = timestamp
+    @data = data
+    @previous_hash = previous_hash
+    @hash = self.hash_block
+  end
+
+  def hash_block
+    Digest::SHA256.hexdigest "#{@index}#{@timestamp}#{@data}#{@previous_hash}"
+  end
+end
+#--------------------------------------------------------------------------
+
+def display_block(block)
+  puts "Index : #{block.index}, timestamp : #{block.timestamp}, data : #{block.data}, previous_hash : #{block.previous_hash}, hash : #{block.hash}", ""
+end
+
+
+def create_new_transaction
+  puts "(1/3) Please enter a sender : "
+  sender = gets.chomp
+  puts "(2/3) Please enter a receiver : "
+  receiver = gets.chomp
+  puts "(3/3) Please enter an amount : "
+  amount = gets.chomp
+
+  Transaction.new(sender, amount, receiver)
+end
+
+def display_transactions(list)
+  if list.empty?
+    puts "There is no yet transactions to be mined.", ""
+  else
+    puts "The current block to mine have the following props : "
+    x = 0
+    puts list
+    list.each do |transaction|
+      puts "(#{x}) Sender : #{transaction.sender}, Receiver : #{transaction.receiver}, Amount : #{transaction.amount}"
+      x += 1
+    end
+  end
+end
+#--------------------------------------------------------------------------
+def create_genesis_block
+  data = {
+      #TODO: Proof of work à definir
+      "proof-of-work" => 1,
+      "genesis" => "block"
+  }
+  genesis_add = Block.new(0, DateTime.now, data, "0")
+  puts "Genesis Block successfully created, with props : "
+  display_block(genesis_add)
+  genesis_add
+end
+#--------------------------------------------------------------------------
+
+
+
+#--------------------------------------------------------------------------
+def proof_of_work(last_proof)
+  incrementer = last_proof + 1
+  until incrementer % 9 == 0 && incrementer % last_proof == 0
+    incrementer += 1
+  end
+  incrementer
+end
+#--------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------
+def mine(last_block, transactions_to_add)
+  last_proof = last_block.data['proof-of-work']
+  proof = proof_of_work(last_proof)
+  transaction_list = []
+  transaction_list << transactions_to_add
+  transaction_list << Transaction.new("network", "1", "miner_address")
+  data = {
+      "proof-of-work" => proof,
+      "transactions" => transaction_list
+  }
+  new_block_index = last_block.index + 1
+  new_block_timestamp = DateTime.now
+
+  Block.new(new_block_index, new_block_timestamp, data, last_block.hash)
+end
+#--------------------------------------------------------------------------
+#
+#blockchain = [create_genesis_block]
+#puts "Genesis Block created"
+#num_of_blocks_to_add = 50
+
+#num_of_blocks_to_add.times do
+#  previous_block = blockchain.last
+#  block_to_add = mine(previous_block)
+#  blockchain << block_to_add
+#  puts "Block #{block_to_add.index} has been added to the blockchain!"
+#  puts "Transaction #{block_to_add.data}"
+#  puts "Hash: #{block_to_add.hash}"
+#  puts ""
+#end
+
---
+++ RubyCoinsBlockchain/main.rb
@@ -0,0 +1,50 @@
+require_relative 'blockchainlib'
+blockchain = []
+
+def main_process
+  puts_menu
+  choice = gets.chomp
+  transactions_to_add = []
+
+  until choice == '4'
+    case choice
+      when '0'
+        blockchain = [create_genesis_block]
+      when '1'
+        puts "Mining a new block"
+        block_to_add = mine(blockchain.last, transactions_to_add)
+        blockchain << block_to_add
+        puts "A new block has been added : "
+        display_block(block_to_add)
+      when '2'
+        puts "Add a new transaction"
+        transactions_to_add << create_new_transaction
+      when '3'
+        display_transactions(transactions_to_add)
+    else
+      puts "Please enter a valid input."
+    end
+    puts_menu
+    choice = gets.chomp
+  end
+
+  save_blockchain = File.open("blocks.txt", 'wb')
+  blockchain.each do |block|
+    save_blockchain.write(Marshal.dump(block))
+  end
+  puts_exit
+end
+
+def puts_menu
+  puts "0 : Create Genesis Block "
+  puts "1 : Mine block"
+  puts "2 : Add a new transaction"
+  puts "3 : Display actual block"
+  puts "4 : Exit program and erase Blockchain"
+end
+
+def puts_exit
+  puts "Exiting..."
+end
+
+main_process
