17448cdf25641cd428ed0c236d46706ac156d9c5
Antoine <aanthime@gmail.com>
2019-04-27 17:24:04 +0200

    Oops. :(

--- RubyCoinsBlockchain/blockchainlib.rb
+++ RubyCoinsBlockchain/blockchainlib.rb
@@ -4,12 +4,18 @@ require_relative 'dblib'
 
 #--------------------------------------------------------------------------
 class Transaction
-  attr_accessor :sender, :amount, :receiver
-  def initialize(sender, amount, receiver)
+  attr_accessor :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
     @sender = sender
-    @amount = amount
     @receiver = receiver
+    @amount = amount
   end
+
+  def formalize
+    "@#{@sender}@#{@receiver}@#{@amount}"
+  end
+
 end
 #--------------------------------------------------------------------------
 
@@ -29,91 +35,27 @@ class Block
   def hash_block
     Digest::SHA256.hexdigest "#{@index}#{@timestamp}#{@data}#{@previous_hash}"
   end
-end
-#--------------------------------------------------------------------------
 
-
-def display_block(block)
-  puts "Index : #{block.index}, timestamp : #{block.timestamp}, proof : #{block.proof} data : #{block.data}, previous_hash : #{block.previous_hash}, hash : #{block.hash}", ''
-end
-#---------------------------------------------------------------------------
-
-def create_new_transaction
-  puts '(1/3) Please enter a sender : '
-  sender = gets.chomp
-  puts '(2/3) Please enter a receiver : '
-  receiver = gets.chomp
-  puts '(3/3) Please enter an amount : '
-  amount = gets.chomp
-
-  Transaction.new(sender, amount, receiver)
-end
-#---------------------------------------------------------------------------
-
-def formalize_transaction(transaction)
-  "@#{transaction.sender}-@#{transaction.receiver}-@#{transaction.amount}"
-end
-#---------------------------------------------------------------------------
-
-def display_transactions(list)
-  if list.empty?
-    puts 'There is no yet transactions to be mined.', ''
-  else
-    puts 'The current block to mine have the following props : '
-    list.each do |transaction|
-      puts formalize_transaction(transaction)
-    end
-  end
-end
-#--------------------------------------------------------------------------
-
-def create_genesis_block
-  data = ['Genesis Block']
-  genesis_add = Block.new(0, DateTime.now, 1, data, '0')
-  puts 'Genesis Block successfully created, with props : '
-  display_block(genesis_add)
-  genesis_add
-end
-#--------------------------------------------------------------------------
-
-
-#--------------------------------------------------------------------------
-def proof_of_work(last_proof)
-  #Ancienne methode de proof of work, elle peut etre utile à un moment,je la laisse meme si non utilisée.
-  incrementer = last_proof + 1
-  until (incrementer % 9).zero? && (incrementer % last_proof).zero?
-    incrementer += 1
-  end
-  incrementer
-end
-#--------------------------------------------------------------------------
-
-def proof_of_work_zeroes_method(last_proof, difficulty)
-  incrementer = rand(1_000_000_000_000_000_000_000_000)
-  result = Digest::SHA256.hexdigest(last_proof.to_s + incrementer.to_s)
-  now = Time.now
-
-  until result[0..difficulty-1] == '0' * difficulty
+  def self.proof_of_work_generation(last_proof, difficulty)
     incrementer = rand(1_000_000_000_000_000_000_000_000)
     result = Digest::SHA256.hexdigest(last_proof.to_s + incrementer.to_s)
-
+    until result[0..difficulty-1] == '0' * difficulty
+      incrementer = rand(1_000_000_000_000_000_000_000_000)
+      result = Digest::SHA256.hexdigest(last_proof.to_s + incrementer.to_s)
+    end
+    incrementer
   end
-  puts "Elapsed time : #{Time.now - now}"
-  incrementer
-end
-#---------------------------------------------------------------------------
 
-def mine(last_block, transactions_to_add, difficulty)
-  last_proof = last_block.proof
-  proof = proof_of_work_zeroes_method(last_proof, difficulty)
-  transaction_list = []
-  transaction_list << transactions_to_add unless transactions_to_add.empty?
-  data = transaction_list
+  def self.launch_mining_process(last_block, transactions_to_add, difficulty)
+    last_proof = last_block.proof
+    proof = self.proof_of_work_generation(last_proof, difficulty)
+    transaction_list = []
+    transaction_list << transactions_to_add unless transactions_to_add.empty?
+    Block.new(last_block.index + 1, DateTime.now, proof, transaction_list, last_block.hash)
+  end
 
-  new_block_index = last_block.index + 1
-  new_block_timestamp = DateTime.now
-  Block.new(new_block_index, new_block_timestamp, proof, data, last_block.hash)
-
+  def display
+    puts "Index : #{index}, timestamp : #{timestamp}, proof : #{proof} data : #{data}, previous_hash : #{previous_hash}, hash : #{hash}", ''
+  end
 end
 #--------------------------------------------------------------------------
-
--- RubyCoinsBlockchain/dblib.rb
+++ RubyCoinsBlockchain/dblib.rb
@@ -1,7 +1,9 @@
 require 'sqlite3'
 
-def add_into_db(block)
-  begin
+# Module destine à l'utilisation de la DB, toutes les opérations
+# de connection à la DB doivent etre faites ici.
+module Database
+  def self.add_into_db(block)
     db = SQLite3::Database.open 'test.db'
 
     # Ajout du bloc dans la table 'Blocks'
@@ -10,34 +12,34 @@ def add_into_db(block)
 
 
     block.data.each do |transaction|
-
+      # Ajout des transactions dans la table 'Transactions'
       transaction.each do |datas|
         query_transaction = "INSERT INTO Transactions VALUES('#{block.index}', '#{datas.sender}', '#{datas.receiver}', '#{datas.amount}')"
         db.execute query_transaction
       end
-
     end
 
-
-    puts 'Block and transactions successfully added into sqlite3'
   rescue SQLite3::Exception => e
     puts 'Unexpected error. Please verify the database state or the query.'
     puts "Error : #{e}"
   ensure
     db.close if db
   end
-end
 
-def get_last
-  begin
-  db = SQLite3::Database.open 'test.db'
-  query = 'SELECT * FROM Blocks WHERE ID = (SELECT MAX(ID) FROM Blocks)'
-  result = db.execute query
-  Block.new(result[0][0], result[0][1], result[0][2], result[0][3], result[0][4])
+
+  # Effectue une requete pour récupérer la dernier élement ajouté
+  def self.get_last
+    db = SQLite3::Database.open 'test.db'
+    query = 'SELECT * FROM Blocks WHERE ID = (SELECT MAX(ID) FROM Blocks)'
+    result = db.execute query
+    Block.new(result[0][0], result[0][1], result[0][2], result[0][3], result[0][4])
   rescue SQLite3::Exception => e
     puts 'Could not load last block in database. Please check the following message for more information : '
     puts "Error : #{e}"
   ensure
     db.close if db
   end
+
 end
+
+
